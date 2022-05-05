8ce3f235b7a4497354f17e5f5e973078d0c3a5d5
anaimhunter <anaimhunter@hotmail.com>
2022-05-04 21:46:43 -0500

    Pure-Task-Inbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { TaskModule } from './components/task.module';
+import { TaskModule } from './components/task/task.module';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { AppComponent } from './app.component';
+import { PureInboxScreenComponent } from './components/pure-inbox-screen/pure-inbox-screen.component';
+import { InboxScreenComponent } from './components/inbox-screen/inbox-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PureInboxScreenComponent,
+    InboxScreenComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/task/task.module.ts
+++ src/app/components/task/task.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxsModule } from '@ngxs/store';
 
-import { TaskComponent } from './task/task.component';
-import { TaskListComponent } from './task-list/task-list.component';
-import { TasksState } from '../state/task.state';
-import { PureTaskListComponent } from './pure-task-list/pure-task-list.component';
+import { TaskComponent } from './task.component';
+import { TaskListComponent } from '../task-list/task-list.component';
+import { TasksState } from '../../state/task.state';
+import { PureTaskListComponent } from '../pure-task-list/pure-task-list.component';
 
 @NgModule({
   imports: [CommonModule, NgxsModule.forFeature([TasksState])],
