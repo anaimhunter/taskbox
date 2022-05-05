ac414317fcd2a284149eb86107175606ba2ce895
anaimhunter <anaimhunter@hotmail.com>
2022-05-04 20:51:34 -0500

    Task-List

---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Task } from '../../models/task.model';
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+  /** The list of tasks */
+  tasksInOrder: Task[] = [];
+
+  /** Checks if it's in loading state */
+  @Input() loading = false;
+
+  /** Event to change the task to pinned */
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  /** Event to change the task to archived */
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+
+  set tasks(arr: Task[]) {
+    this.tasksInOrder = [
+      ...arr.filter(t => t.state === 'TASK_PINNED'),
+      ...arr.filter(t => t.state !== 'TASK_PINNED'),
+    ];
+  }
+}
---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Task } from '../../models/task.model';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+  @Input() task: Task;
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+
+  onPin(id: any) {
+    this.onPinTask.emit(id);
+  }
+
+  onArchive(id: any) {
+    this.onArchiveTask.emit(id);
+  }
+}
